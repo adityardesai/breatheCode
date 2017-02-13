79d352de1bc175f5205f8d9c041454f2f4c6f4cb
adityardesai <adityardesai@gmail.com>
2017-02-13 15:07:51 -0800

    hashing

---
+++ src/org/usc/edu/hash/Frequency.java
@@ -0,0 +1,16 @@
+package org.usc.edu.hash;
+
+/*
+ * 
+ * Input :  arr[] = { 3, 1, 3, 2, 3, 2 }.
+Output : 2
+Frequency of 3 = 3.
+Frequency of 2 = 2.
+Frequency of 1 = 1.
+Here difference of frequency of element 3 and 1 is = 3 - 1 = 2.
+Also 3 > 1.
+ * */
+
+public class Frequency {
+
+}
