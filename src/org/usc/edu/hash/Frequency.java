4eb7ddab789f839545b5c6fbfae08f1c92e4ef06
adityardesai <adityardesai@gmail.com>
2017-02-13 16:24:40 -0800

    GFG

--- src/org/usc/edu/hash/Frequency.java
+++ src/org/usc/edu/hash/Frequency.java
@@ -1,5 +1,10 @@
 package org.usc.edu.hash;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * 
  * Input :  arr[] = { 3, 1, 3, 2, 3, 2 }.
@@ -12,5 +17,24 @@ Also 3 > 1.
  * */
 
 public class Frequency {
+
+	public static void main(String[] args) {
+		int myArray[] = { 3, 1, 3, 2, 3, 2 };
+		Map<Integer, Integer> myHashMap = new HashMap();
+		for (int i : myArray) {
+			if (myHashMap.get(i) != null) {
+				myHashMap.put(i, (int) myHashMap.get(i) + 1);
+			} else {
+				myHashMap.put(i, 1);
+			}
+		}
+		ArrayList<Integer> arList = new ArrayList<Integer>();
+		for (Map.Entry<Integer, Integer> map : myHashMap.entrySet()) {
+			arList.add(map.getValue());
+		}
+		arList.sort(null);
+		System.out.println(arList.get(arList.size()) - arList.get(0));
+		
+	}
 
 }
